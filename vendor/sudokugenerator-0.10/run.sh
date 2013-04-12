72563ec2d9de9950bcd9a472f20e1304c70fafbd
Joel Teichroeb <joel@teichroeb.net>
2013-04-12 14:13:47 -0700

    more run improvments

--- vendor/sudokugenerator-0.10/run.sh
+++ vendor/sudokugenerator-0.10/run.sh
@@ -2,7 +2,9 @@
 ../../fixformat < sudoku-${1}x${1}.pls > puzzle
 ../../sudoku2sat < puzzle > cnf1
 ../../sudoku2sat2 < puzzle > cnf2
-time="/usr/bin/time -f %e timeout 15"
+../../sgen < puzzle > out.S
+echo $3 >> data.txt
+time="/usr/bin/time -f %e timeout 10"
 #echo "cnf1 + minisat"
 $time ../../vendor/minisat/core/minisat < cnf1 > /dev/null 2>> data.txt
 #echo "cnf2 + minisat"
@@ -16,6 +18,8 @@ $time ../../vendor/lingeling-ala-b02aa1a-121013/lingeling < cnf1 > /dev/null 2>>
 #echo "cnf2 + lingeling"
 $time ../../vendor/lingeling-ala-b02aa1a-121013/lingeling < cnf2 > /dev/null 2>> data.txt
 #echo "direct"
+$time ../../vendor/clasp-2.1.3/build/release/bin/clasp < cnf1 > /dev/null 2>> data.txt
+$time ../../vendor/clasp-2.1.3/build/release/bin/clasp < cnf2 > /dev/null 2>> data.txt
 $time ../../direct < puzzle > /dev/null 2>> data.txt
-
+$time ~/Enfragmo ../../S.T out.S > /dev/null 2>> data.txt
 
