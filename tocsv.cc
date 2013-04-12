de772f17e562e1d27ea1b2a2a6db9413f5c2a3e0
Joel Teichroeb <joel@teichroeb.net>
2013-04-12 16:49:57 -0700

    tocsv

--- tocsv.cc
+++ tocsv.cc
@@ -13,18 +13,21 @@ int main(int argc, const char **argv)
     int column = 0;
 
     char buffer[1024];
+    int line = 0;
+    cout << line << ",";
     while (!cin.eof()) {
         cin.getline(buffer, 1024);
         if (cin.eof()) {
             break;
         }
-        if (buffer[0] == 'C') {
+        if (buffer[0] == 'C' || buffer[0] == '*') {
             continue;
         }
         cout << buffer;
         ++column;
         if (column == columns) {
-            cout << endl;
+            ++line;
+            cout << endl << line << ",";
             column = 0;
         } else {
             cout << ",";
